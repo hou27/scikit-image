b90b4628da751ff0a7b44b9fe255b00f5acf9d10
hou27 <ataj125@gmail.com>
2023-05-24 01:44:40 +0900

    contour 검출 함수 추가
    유의미한 성능 개선은 없었음

--- structural-similarity.py
+++ structural-similarity.py
@@ -2,7 +2,17 @@ import cv2
 from skimage.metrics import structural_similarity as ssim
 import matplotlib.pyplot as plt
 
-# TODO add contour detection for enhanced accuracy
+# 컨투어 검출 함수
+def detect_contours(image):
+    # 이미지를 그레이스케일로 변환
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # 가장자리 검출
+    edges = cv2.Canny(gray, 50, 150)
+    # 컨투어 검출
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # 컨투어 그리기
+    cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+    return image
 
 def imshow(imgs):
     # 이미지 출력
